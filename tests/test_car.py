6efa3452160bb20a0bab292b8b30bf59360ae12a
ben31009 <benyamin.ittah@hotmail.fr>
2021-10-05 20:08:03 +0200

    Update test_car.py

--- tests/test_car.py
+++ tests/test_car.py
@@ -13,6 +13,23 @@ class CarTestCase(unittest.TestCase):
         self.assertEqual(car.current_speed, 0)
         self.assertEqual(car.motor, Motor(0))
 
+    def test_getters(self):
+        car = Car(current_speed=100, horse_power=150)
+        self.assertEqual(car.horse_power, 100)
+        self.assertEqual(car.current_speed, 150)
+
+    def test_setters(self):
+        car = Car(current_speed=100, horse_power=150)
+        car.horse_power = 200
+        self.assertEqual(car.horse_power, 200)
+        self.assertEqual(car.current_speed, 100)
+        car.current_speed = 120
+        self.assertEqual(car.current_speed, 120)
+        car.horse_power = -2
+        car.current_speed = -2
+        self.assertEqual(car.horse_power,200)
+        self.assertEqual(car.current_speed, 120)
+
 
 if __name__ == "__main__":
     unittest.main()
